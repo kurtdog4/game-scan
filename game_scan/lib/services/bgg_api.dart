d56ae5d9c21cc2156e136773c0712493ada121dc
Kurtis Campbell <kurtdog4@gmail.com>
2022-09-23 15:13:01 -0600

    Added BGG API. Added dark theme.

---
+++ game_scan/lib/services/bgg_api.dart
@@ -0,0 +1,6 @@
+const String _baseApi = "https://www.boardgamegeek.com/xmlapi/";
+const String _boardgameApi = "${_baseApi}boardgame/";
+const String getGloomhavenInfo = "${_boardgameApi}174430/";
+const String getBrassBirminghamInfo = "${_boardgameApi}224517/";
+const String getArkNovaInfo = "${_boardgameApi}342942";
+const String getPandemicLegacyS1Info = "${_boardgameApi}161936";
